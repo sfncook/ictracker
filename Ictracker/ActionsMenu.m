a7116bbb91f38e76394b21de749dcbbc969754d5
S Cook <sfncook@gmail.com>
2014-01-13 21:04:33 -0800

    Add actions.

--- Ictracker/ActionsMenu.m
+++ Ictracker/ActionsMenu.m
@@ -13,12 +13,29 @@
 
 - (id)init
 {
-    NSArray *actionNames = @[@"E201",
-                             @"E202",
-                             @"E203",
-                             @"E204",
-                             @"E205",
-                             @"E206"];
+    NSArray *actionNames = @[
+                             @"Search & Rescue",
+                             @"Fire Control",
+                             @"Secure Utilities",
+                             @"Vertical Ventilation",
+                             @"Horizontal Ventilation",
+                             @"Open the Building",
+                             @"Soften the Building",
+                             @"360 of the Building",
+                             @"Recon",
+                             @"Supply Line",
+                             @"Take a Line",
+                             @"1-1/2\"",
+                             @"1-3/4\"",
+                             @"2\"",
+                             @"2-1/2\"",
+                             @"3\"",
+                             @"Portable Master Stream",
+                             @"Elevated Master Stream",
+                             @"Deck Gun",
+                             @"Charge the Sprinkler System",
+                             @"Charge the Stand Pipe",
+                             @"Fan to the Door"];
     
     self = [super initWithItems:actionNames];
     if (self) {
