cc90055eab82262398b9e9b556108bccc04f6562
S Cook <sfncook@gmail.com>
2014-01-25 10:30:35 -0800

    Add TransactionLogger

---
+++ Ictracker/TransactionLogger.h
@@ -0,0 +1,18 @@
+//
+//  TransactionLogger.h
+//  Ictracker
+//
+//  Created by S. Cook on 1/25/14.
+//  Copyright (c) 2014 cook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Transaction.h"
+
+@interface TransactionLogger : NSObject
+
+@property (readonly, nonatomic, retain) NSMutableArray* transactions;
+
+- (void) addTransaction:(Transaction*)transaction;
+
+@end
---
+++ Ictracker/TransactionLogger.m
@@ -0,0 +1,39 @@
+//
+//  TransactionLogger.m
+//  Ictracker
+//
+//  Created by S. Cook on 1/25/14.
+//  Copyright (c) 2014 cook. All rights reserved.
+//
+
+#import "TransactionLogger.h"
+
+@implementation TransactionLogger
+
++ (TransactionLogger *)transLogger
+{
+    static TransactionLogger *transLogger;
+    
+    @synchronized(self)
+    {
+        if (!transLogger)
+            transLogger = [[TransactionLogger alloc] init];
+        
+        return transLogger;
+    }
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        _transactions = [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+- (void) addTransaction:(Transaction*)transaction {
+    
+}
+
+@end
