b0771e16287e22083020101b217340b5997ebba6
S Cook <sfncook@gmail.com>
2014-01-26 15:23:31 -0800

    Add SplashView and large ICTLogo

---
+++ Ictracker/SplashView.h
@@ -0,0 +1,13 @@
+//
+//  SplashView.h
+//  Ictracker
+//
+//  Created by S. Cook on 1/26/14.
+//  Copyright (c) 2014 cook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplashView : UIView
+
+@end
