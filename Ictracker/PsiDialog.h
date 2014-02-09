605ce6bea521eecf47f4aea806dd16f7cf6ddf77
S Cook <sfncook@gmail.com>
2014-02-08 17:33:59 -0800

    First draft of picker for PSI. Very rough.

--- Ictracker/PsiDialog.h
+++ Ictracker/PsiDialog.h
@@ -11,12 +11,15 @@
 #import "ButtonClickDelegate.h"
 #import "PsiDialogDelegate.h"
 
-@interface PsiDialog : UIView<ButtonClickDelegate>
+@interface PsiDialog : UIView<ButtonClickDelegate, UIPickerViewDelegate>
 
 @property (readonly, retain, nonatomic) ButtonView* windowCancelButton;
 @property (readonly, retain, nonatomic) id<PsiDialogDelegate> delegate;
+@property (readonly, retain, nonatomic) NSArray* psiValues;
+@property (readonly, retain, nonatomic) UIPickerView* picker;
 
 - (id)initWithDelegate:(id<PsiDialogDelegate>)delegate;
 - (void) setSelectedPsi:(NSString*)psi;
+- (void) setPosition:(CGPoint)position;
 
 @end
--- Ictracker/PsiDialog.m
+++ Ictracker/PsiDialog.m
@@ -21,53 +21,34 @@
         
         _windowCancelButton = [[ButtonView alloc] initWithName:@"" delegate:self size:BUTTON_BLANK_WINDOW];
         [_windowCancelButton setPosition:CGPointMake(0.0,0.0)];
-        
-        CGSize size = CGSizeMake([Utils millimetersToPixels:61.5], [Utils millimetersToPixels:48]);
-        UIView* dialogBg = [[UIView alloc] initWithFrame:CGRectMake(
-                                                                    [Utils windowHeight]/2-size.width/2,
-                                                                    [Utils windowWidth]/2-size.height/2,
-                                                                    size.width,
-                                                                    size.height)];
-        dialogBg.backgroundColor = [UIColor lightGrayColor];
-        dialogBg.layer.borderColor = [UIColor darkGrayColor].CGColor;
-        dialogBg.layer.borderWidth = 3.0;
-        dialogBg.layer.cornerRadius = 5.0;
-        
         [self addSubview:_windowCancelButton];
-        [self addSubview:dialogBg];
-        
-        float orig_x = dialogBg.frame.origin.x+[Utils millimetersToPixels:1.5];
-        float orig_y = dialogBg.frame.origin.y+[Utils millimetersToPixels:1.5];
-        float x = orig_x;
-        float y = orig_y;
+
+        NSMutableArray* psiValuesTmp = [[NSMutableArray alloc] init];
         for (int psi=4500; psi>=0; psi-=100) {
-            ButtonView* btn = [[ButtonView alloc] initWithName:[[NSNumber numberWithInt:psi] stringValue] delegate:self size:BUTTON_SMALL];
-            [btn setPosition:CGPointMake(x, y)];
-            [self addSubview:btn];
-            x+=btn.frame.size.width+[Utils millimetersToPixels:1.5];
-            
-            //Check if end of column
-            float next_x = x+btn.frame.size.width+[Utils millimetersToPixels:1.5];
-            if (next_x>dialogBg.frame.origin.x+dialogBg.frame.size.width) {
-                x=orig_x;
-                y+=btn.frame.size.height+[Utils millimetersToPixels:1.5];
-            }
-            
-            //Change color of button
-            if (psi<1500) {
-                [btn setNormalColor:[UIColor redColor]];
-            } else if (psi<3000) {
-                [btn setNormalColor:[UIColor yellowColor]];
-            } else {
-                [btn setNormalColor:[UIColor greenColor]];
-            }
+            [psiValuesTmp addObject:[NSNumber numberWithInt:psi]];
         }//for
+        _psiValues = [[NSArray alloc] initWithArray:psiValuesTmp];
+        
+        _picker = [[UIPickerView alloc] initWithFrame:CGRectMake(540, 10, [Utils millimetersToPixels:7], 100)];
+        _picker.delegate = self;
+        _picker.showsSelectionIndicator = NO;
+//        _picker.backgroundColor = [UIColor blueColor];
+        [self addSubview:_picker];
     }
     return self;
 }
 
 - (void) setSelectedPsi:(NSString*)psi {
+    if (psi.length==0) {
+        return;
+    }
     
+    for (NSNumber* psiNum in _psiValues) {
+        if ([psi isEqualToString:psiNum.stringValue]) {
+            [_picker selectRow:[_psiValues indexOfObject:psiNum] inComponent:0 animated:NO];
+            return;
+        }
+    }
 }
 
 //*** ButtonClickDelegate ***
@@ -75,11 +56,52 @@
     self.hidden = YES;
     if (_windowCancelButton==selector){
         [_delegate cancel];
-    } else {
-        ButtonView* btn = selector;
-        NSString* psi = btn.name;
-        [_delegate selectPsi:psi];
     }
+}
+
+
+//*** UIPickerViewDelegate ***
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow: (NSInteger)row inComponent:(NSInteger)component {
+    NSNumber* psiNum = [_psiValues objectAtIndex:row];
+    [_delegate selectPsi:psiNum.stringValue];
+}
+
+// tell the picker how many rows are available for a given component
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return _psiValues.count;
+}
+
+// tell the picker how many components it will have
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
+}
+
+- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view {
+    NSNumber* psiNum = [_psiValues objectAtIndex:row];
+    int psi = [psiNum intValue];
+    
+    UILabel* label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, [Utils millimetersToPixels:7], [Utils millimetersToPixels:5])];
+    [label setText:psiNum.stringValue];
+    label.font = [UIFont fontWithName:@"Arial" size:14];
+    if (psi<1500) {
+        [label setBackgroundColor:[UIColor redColor]];
+    } else if (psi<3000) {
+        [label setBackgroundColor:[UIColor yellowColor]];
+    } else {
+        [label setBackgroundColor:[UIColor greenColor]];
+    }
+    
+    return label;
+}
+
+- (void) setPosition:(CGPoint)position {
+    self.frame = CGRectMake(position.x, position.y, self.frame.size.width, self.frame.size.height);
+}
+
+// tell the picker the width of each row for a given component
+- (CGFloat)pickerView:(UIPickerView *)pickerView widthForComponent:(NSInteger)component {
+    return [Utils millimetersToPixels:7];
 }
 
 @end
